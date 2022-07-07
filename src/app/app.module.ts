f4a993ec4410342ea02000533b047c309bbdc7ad
ANDZANU <oblyn.mathebula@gmail.com>
2022-07-07 23:17:11 +0200

    Warning and successful alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SuccessfulComponent } from './successful/successful.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningComponent,
+    SuccessfulComponent
+    
   ],
   imports: [
     BrowserModule,
---
+++ src/app/successful/successful.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-successful',
+    templateUrl:'./successful.component.html',
+    styles:[`
+    p{
+        color:green;
+    }
+
+    `]
+})
+
+export class SuccessfulComponent{
+    
+}
